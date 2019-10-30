3683f04a19fa343d48ee0e1b53e7cc1fcc62624d
Saadh S <atextbooksituation@gmail.com>
2019-10-30 19:00:23 -0400

    Not sure if supposed to commit this Netlify file.
    - Trying to get React Router working on Netlify
    - Added other links to footer and modified rando text

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = ( props ) => (
       <div className="footer__container">
         <p className="footer__dashes"> -- </p>
         <p className="footer__content">
-          The footer. <a className="lighter-links" href="https://atextbooksituation.com">Saadh Shehzad</a>. 2019. Do what you want with this site :).
+          The footer. <a className="lighter-links" href="https://atextbooksituation.com">Saadh Shehzad</a>. 2019. Do what you want with this site :). <a className="lighter-links" href="https://github.com/inoicouldalwaysturn2u/collaborative-multiple-timers-cuckoo">Github repo</a>. <a className="lighter-links" href="/about">About & How-To</a>. <a className="lighter-links" href="/privacy-policy">Privacy Policy</a>.
         </p>
       </div>
     </div>
--- src/frontend/About/index.js
+++ src/frontend/About/index.js
@@ -3,12 +3,16 @@ import React from 'react';
 const AboutPage = () => {
   return (
     <div>
+      <h2>About</h2>
       <p>
         Simple description of the site. Displays <a href="https://cuckoo.team">Cuckoo.team</a> timers. Can have different timers grouped together. No login. Groups of timers are saved and can go back to them by their name.
       </p>
       <p>
-        Tech is React JS. Firebase's Firestore for the minimal data holding. Open sourced code available at <a href="https://github.com/inoicouldalwaysturn2u/collaborative-timers-cuckoo">Github</a> or <a href="https://github.com/inoicouldalwaysturn2u/collaborative-timers-cuckoo">Gitlab</a>.
+        Tech is React JS. Firebase's Firestore for the minimal data holding. Open sourced code available at <a href="https://github.com/inoicouldalwaysturn2u/collaborative-multiple-timers-cuckoo">Github</a> or <a href="https://gitlab.com/inoicouldalwaysturn2u/collaborative-multiple-timers-cuckoo">Gitlab</a>.
       </p>
+
+      <h2>How To</h2>
+      <p>Yes</p>
     </div>
   );
 };
